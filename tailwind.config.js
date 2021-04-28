ec6a005a297c511f8f504f49a50e2d896a75fe54
Jerson Moreno <jersonmr@gmail.com>
2021-04-28 12:41:24 -0400

    Refactor all the project with tailwindcss and alpinejs

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+const { colors } = require('tailwindcss/defaulttheme');
+
+module.exports = {
+  purge: ['./**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      main: '#101010',
+      body: '#444444',
+      lightBorder: '#aaa',
+      white: '#fff',
+      red: colors.red,
+      blue: colors.blue
+    },
+    container: {
+      center: true,
+      padding: '15px'
+    },
+    extend: {
+      fontFamily: {
+        'body': ['Roboto Mono', 'monospace'],
+        'title': ['Lato', 'sans-serif'],
+      },
+      fontSize: {
+        big: '12rem',
+        '10xl': '10rem',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      cursor: ['disabled'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
